7a1659931018edcce1e2d21a01ec50a3b129049c
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-18 20:04:20 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -98,22 +98,22 @@ export default function Home() {
 		<>
 		<Card className="flex-col items-center justify-center p-4 w-full">
 			<h1 className="text-center">시간표</h1>
-			<div className="border-blue-300 border w-full">
+			<div className="w-full max-w-[1000px]">
 				<div className="grid grid-cols-5 text-center">
-						<span className="text-sm text-center p-2 border-blue-300 border">월</span>
-						<span className="text-sm text-center p-2 border-blue-300 border">화</span>
-						<span className="text-sm text-center p-2 border-blue-300 border">수</span>
-						<span className="text-sm text-center p-2 border-blue-300 border">목</span>
-						<span className="text-sm text-center p-2 border-blue-300 border">금</span>
+						<span className="text-sm text-center p-2 bg-[#0070F0] text-white border shadow-xl rounded-xl">월</span>
+						<span className="text-sm text-center p-2 bg-[#0070F0] text-white border shadow-xl rounded-xl">화</span>
+						<span className="text-sm text-center p-2 bg-[#0070F0] text-white border shadow-xl rounded-xl">수</span>
+						<span className="text-sm text-center p-2 bg-[#0070F0] text-white border shadow-xl rounded-xl">목</span>
+						<span className="text-sm text-center p-2 bg-[#0070F0] text-white border shadow-xl rounded-xl">금</span>
 				</div>
 				{
 					[1,2,3,4,5,6,7,8,9,10].map((item) => (
 						<div className="grid grid-cols-5 text-center" key={item}>
-								<div className="text-sm text-center sm:p-2 border-blue-300 border">{ timeTable['월'].some(time => time.time == item) ? timeTable['월'].find(time => time.time == item).lecture.name : '-' }</div>
-								<div className="text-sm text-center sm:p-2 border-blue-300 border">{ timeTable['화'].some(time => time.time == item) ? timeTable['화'].find(time => time.time == item).lecture.name : '-' }</div>
-								<div className="text-sm text-center sm:p-2 border-blue-300 border">{ timeTable['수'].some(time => time.time == item) ? timeTable['수'].find(time => time.time == item).lecture.name : '-' }</div>
-								<div className="text-sm text-center sm:p-2 border-blue-300 border">{ timeTable['목'].some(time => time.time == item) ? timeTable['목'].find(time => time.time == item).lecture.name : '-' }</div>
-								<div className="text-sm text-center sm:p-2 border-blue-300 border">{ timeTable['금'].some(time => time.time == item) ? timeTable['금'].find(time => time.time == item).lecture.name : '-' }</div>
+								{ timeTable['월'].some(time => time.time == item) ? (<div className="text-sm text-center p-1 sm:p-2 bg-[#0070F0] text-white border border-gray-300 shadow-2xl rounded-xl">{timeTable['월'].find(time => time.time == item).lecture.name}</div>) : (<div className="text-sm text-center p-2 bg-gray-100 border border-gray-300 shadow-2xl rounded-xl"></div>) }
+								{ timeTable['화'].some(time => time.time == item) ? (<div className="text-sm text-center p-1 sm:p-2 bg-[#0070F0] text-white border border-gray-300 shadow-2xl rounded-xl">{timeTable['화'].find(time => time.time == item).lecture.name}</div>) : (<div className="text-sm text-center p-2 bg-gray-100 border border-gray-300 shadow-2xl rounded-xl"></div>) }
+								{ timeTable['수'].some(time => time.time == item) ? (<div className="text-sm text-center p-1 sm:p-2 bg-[#0070F0] text-white border border-gray-300 shadow-2xl rounded-xl">{timeTable['수'].find(time => time.time == item).lecture.name}</div>) : (<div className="text-sm text-center p-2 bg-gray-100 border border-gray-300 shadow-2xl rounded-xl"></div>) }
+								{ timeTable['목'].some(time => time.time == item) ? (<div className="text-sm text-center p-1 sm:p-2 bg-[#0070F0] text-white border border-gray-300 shadow-2xl rounded-xl">{timeTable['목'].find(time => time.time == item).lecture.name}</div>) : (<div className="text-sm text-center p-2 bg-gray-100 border border-gray-300 shadow-2xl rounded-xl"></div>) }
+								{ timeTable['금'].some(time => time.time == item) ? (<div className="text-sm text-center p-1 sm:p-2 bg-[#0070F0] text-white border border-gray-300 shadow-2xl rounded-xl">{timeTable['금'].find(time => time.time == item).lecture.name}</div>) : (<div className="text-sm text-center p-2 bg-gray-100 border border-gray-300 shadow-2xl rounded-xl"></div>) }
 						</div>
 					))
 				}
