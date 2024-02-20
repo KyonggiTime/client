cb30fc73fc3db8e9d5a0c31c7cf16a172e3bb5a7
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-21 00:06:34 +0900

    feat: 로고 클릭시 시간표 화면으로 이동

--- components/header-bar.component.tsx
+++ components/header-bar.component.tsx
@@ -2,12 +2,19 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle } from "@nextui-org/navbar";
 import Link from "next/link";
 import { HeaderNavigation } from "./header-navigation";
+import { useRouter } from "next/navigation";
 
 export const HeaderBar = () => {
+    const { push } = useRouter();
+
     return (
       <Navbar className="shadow-md">
-        <NavbarBrand className="cursor-pointer">
-          <p className="font-bold text-inherit pointer-events-none select-none">경기타임</p>
+        <NavbarBrand className="cursor-pointer" onClick={() => push('/')}>
+          <p className="font-bold text-inherit pointer-events-none select-none">
+            <Link className="font-bold text-md" color="foreground" href="/">
+                경기타임
+            </Link>
+          </p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4 select-none" justify="center">
           <NavbarItem>
