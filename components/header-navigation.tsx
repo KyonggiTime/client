8602da16ab2f9126d3a5a2a02fc9238629103147
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-21 11:35:08 +0900

    feat: call prompt

--- components/header-navigation.tsx
+++ components/header-navigation.tsx
@@ -1,8 +1,28 @@
 'use client';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle } from "@nextui-org/navbar";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const HeaderNavigation = () => {
+    const [prompt, setPrompt] = useState<unknown>();
+
+    useEffect(() => {
+      window.addEventListener('beforeinstallprompt', (event) => {
+          event.preventDefault();
+          console.log(event.prompt)
+          if (event && event.prompt) {
+            setPrompt(event);
+          }
+      });
+    })
+
+    const onClick = () => {
+      console.log(prompt);
+      if (prompt != null) {
+        prompt.prompt();
+      }
+    }
+
     return (
       <>
         <Link className="font-bold text-sm mr-6" color="foreground" href="/">
@@ -14,8 +34,11 @@ export const HeaderNavigation = () => {
         <Link className="font-bold text-sm mr-6" color="foreground" href="/notice">
             공지사항
         </Link>
-        <Link className="font-bold text-sm" color="foreground" href="/help">
+        <Link className="font-bold text-sm mr-6" color="foreground" href="/help">
             버그 제보 및 문의
+        </Link>
+        <Link className="font-bold text-sm" color="foreground" href="" onClick={onClick}>
+            앱 설치
         </Link>
       </>
     );
