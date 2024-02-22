f7e36ad3dacda0f26b6330e63af6724664268b07
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-22 22:41:07 +0900

    chore: notice

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -10,27 +10,12 @@ export default function Notice() {
 			<h2 className="text-red-500">잘못 기입된 강의가 존재할 수 있습니다. 시간표를 다 만드시고 나서 꼭 수강신청 사이트에서 확인해주세요!!</h2>
 		</Card>
 		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
-			<h1 className="text-center font-bold">경기타임 Next.js 팀원 모집</h1>
-			<a className="text-blue-600 underline" href="https://everytime.kr/375140/v/331630405">모집 글</a><br/>
+			<h1 className="text-center font-bold">경기타임 Next.js 팀원 모집 <a className="text-blue-600 underline" href="https://everytime.kr/375140/v/331630405">모집 글</a></h1>
 		</Card>
 		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
 			<h1 className="text-center font-bold">업데이트 예정</h1>
 			<h1 className="text-center">구글 계정과 학점계산기 동기화 (현재 작업중)</h1>
 			<h1 className="text-center">시간표 여러 개 저장</h1>
-		</Card>
-		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
-			<h1 className="text-center font-bold">경기타임 개발자 후원</h1>
-			<h1 className="text-center">후원해주시는 모든 금액은 서버비(모두 제가 사비로 내고 있습니다.)와 코드를 짜내기위해 구입하는 컴포즈커피 아메리카노에 사용됩니다 ㅠㅠ</h1>
-			<h1 className="text-center">
-				<a className="text-blue-600 underline" href="https://www.buymeacoffee.com/jinhyeokfang">Buy Me a Coffee</a><br/>
-			</h1>
-		</Card>
-		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
-			<h1 className="text-center font-bold">안녕하세요 경기타임 개발자입니다.</h1>
-			<h1 className="text-center">안녕하세요 시간표 작성 사이트 경기타임 개발자입니다. 감사 인사 및 버그 제보를 받기 위해 글을 썼습니다. <br/>
-2~4학년 수강신청 기간이 종료되었음에도 경기타임의 사용자 수가 최고치(하루 방문자수 천 명 돌파)를 경신했습니다. 왜 그런가 하고 살펴보니 신입생 분들이 시간표 작성에 불편을 겪어 많이 이용을 해주고 계신 것이었습니다. (학번이 안나오면 쿠티스를 못쓰는 걸 몰랐습니다. 쿠티스 없이 시간표를 짜다니...) <br/>
-경기타임에 부족함이 많음에도 이용해주신 모든 학우분께 정말로 감사합니다! 특히나 사용하시다 불편한 점을 제보해주신 분들껜 더더욱 감사드립니다. (에타에 올라온 글 모두 확인했고 수정 예정입니다.) <br/>
-경기타임을 이용해주시는 많은 학우분들께 보답하고자 버그와 불편한 점을 제보 받아 사용성을 더욱 개선하고자 합니다! 문의 페이지를 통해 불편한 점을 제보해주시면 빠른 시일내에 개선하도록 하겠습니다. 경기타임을 이용해주셔서 정말 정말 감사합니다!</h1>
 		</Card>
 		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
 			<h1 className="text-center font-bold">경기타임 개발과정 후기</h1>
@@ -41,16 +26,6 @@ export default function Notice() {
 		</Card>
 		<Card className="flex-col items-center justify-center p-4 mt-2 mb-2 w-full">
 			<h1 className="text-center font-bold">업데이트 내역</h1>
-			<h1 className="text-center">텍스트 가독성 / 디자인 개선</h1>
-			<h1 className="text-center">띄어쓰기 시 강의가 검색되지 않는 오류 수정</h1>
-			<h1 className="text-center">학년 조건 입력 시 나머지 조건이 무시되는 오류 수정</h1>
-			<h1 className="text-center">API 서버 배포과정 수정 (hotfix)</h1>
-			<h1 className="text-center">공지사항 페이지 추가</h1>
-			<h1 className="text-center">학점계산기 페이지 추가</h1>
-			<h1 className="text-center">문의 페이지 추가</h1>
-			<h1 className="text-center">학년 미입력시 전공 과목이 뜨지 않는 오류 수정 (hotfix)</h1>
-			<h1 className="text-center">4.5L 등 90분 수업이 입력되지 않는 오류 수정</h1>
-			<h1 className="text-center">강의번호에 과목코드 병기</h1>
 			<h1 className="text-center">구글 계정으로 시간표 동기화 (Alpha)</h1>
 			<h1 className="text-center">시간표가 제대로 저장되지 않는 문제 수정</h1>
 			<h1 className="text-center">계정에 시간표 정보가 없는 경우 로컬의 시간표를 업로드하여 동기화</h1>
