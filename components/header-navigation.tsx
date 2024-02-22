f5d6f57c7d77a30c764671d73e7f6a15f459da33
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-22 14:31:07 +0900

    feat: 로그인 시 로그인 버튼 숨김

--- components/header-navigation.tsx
+++ components/header-navigation.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { authState } from "@/states/auth";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle } from "@nextui-org/navbar";
 import Link from "next/link";
+import { useRecoilState } from "recoil";
 
 export const HeaderNavigation = () => {
+    const [auth, setAuth] = useRecoilState(authState);
     return (
       <>
         <Link className="font-bold text-sm mr-6" color="foreground" href="/">
@@ -17,9 +20,12 @@ export const HeaderNavigation = () => {
         <Link className="font-bold text-sm mr-6" color="foreground" href="/help">
             버그 제보 및 문의
         </Link>
-        <Link className="font-bold text-sm" color="foreground" href="https://api.kyonggiti.me/google">
-            구글로 로그인 (불안정)
-        </Link>
+        {
+          auth.isLoggedIn ||
+          <Link className="font-bold text-sm" color="foreground" href="https://api.kyonggiti.me/google">
+              구글로 로그인 (Alpha)
+          </Link>
+        }
       </>
     );
 }
