afec852bbe5e203d2dd49080fc01247db5e7d73d
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-23 12:14:48 +0900

    feat: 평점계산기 표시 개선

---
+++ config/mark.ts
@@ -0,0 +1,46 @@
+export const MARK = [
+  {
+    key: "4.5",
+    label: "A+",
+  },
+  {
+    key: "4",
+    label: "A0",
+  },
+  {
+    key: "3.5",
+    label: "B+",
+  },
+  {
+    key: "3",
+    label: "B0",
+  },
+  {
+    key: "2.5",
+    label: "C+",
+  },
+  {
+    key: "2",
+    label: "C0",
+  },
+  {
+    key: "1.5",
+    label: "D+",
+  },
+  {
+    key: "1",
+    label: "D0",
+  },
+  {
+    key: "0",
+    label: "F",
+  },
+  {
+    key: "-1",
+    label: "Pass",
+  },
+  {
+    key: "-1",
+    label: "Non Pass",
+  },
+];
