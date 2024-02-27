384e9384cdc0e7547f7c7816ea038f25cb22d8d2
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-27 20:35:19 +0900

    공지, 문의 페이지 통일

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,22 @@ export default function Home() {
 			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/calculator')}>
 				<h1 className="text-2xl">학점계산기</h1>
 			</Card>
+			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-200 text-white shadow-2xl">
+				<h1 className="text-2xl">강의평가 (추가 예정)</h1>
+			</Card>
+		</div>
+		<Card className="flex-col items-center justify-center my-6 w-full">
+			<h1 className="text-center font-bold text-md">어플리케이션 설치 방법 (모바일)</h1>
+			<h1 className="text-center text-md">아이폰: 사파리로 접속 -&gt; 공유 아이콘 클릭 -&gt; 홈 화면에 추가</h1>
+			<h1 className="text-center text-md">안드로이드: 크롬으로 접속 -&gt; 브라우저 우측 메뉴 -&gt; 앱 설치</h1>
+		</Card>
+		<Card className="flex-col items-center justify-center my-6 w-full">
+			<h1 className="text-center font-bold text-md">로그인하여 시간표를 휴대폰 / PC와 동기화하세요!</h1>
+		</Card>
+		<div className="flex flex-wrap gap-4 justify-center">
+			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/help')}>
+				<h1 className="text-2xl">공지사항 및 문의</h1>
+			</Card>
 			{
 				auth.isLoggedIn ?
 				(
@@ -34,22 +50,6 @@ export default function Home() {
 					</Card>
 				)
 			}
-		</div>
-		<Card className="flex-col items-center justify-center my-6 w-full">
-			<h1 className="text-center font-bold text-md">어플리케이션 설치 방법 (모바일)</h1>
-			<h1 className="text-center text-md">아이폰: 사파리로 접속 -&gt; 공유 아이콘 클릭 -&gt; 홈 화면에 추가</h1>
-			<h1 className="text-center text-md">안드로이드: 크롬으로 접속 -&gt; 브라우저 우측 메뉴 -&gt; 앱 설치</h1>
-		</Card>
-		<Card className="flex-col items-center justify-center my-6 w-full">
-			<h1 className="text-center font-bold text-md">로그인하여 시간표를 휴대폰 / PC와 동기화하세요!</h1>
-		</Card>
-		<div className="flex flex-wrap gap-4 justify-center">
-			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/notice')}>
-				<h1 className="text-2xl">공지사항</h1>
-			</Card>
-			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/help')}>
-				<h1 className="text-2xl">문의</h1>
-			</Card>
 		</div>
 		</>
 	);
