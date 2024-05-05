499b16997e6373f29982b6ff806e3035b3ae0a88
JinhyeokBang <jinhyeokfang@gmail.com>
2024-05-05 11:30:17 +0900

    fix: 표시버그수정

--- components/evaluation.component.tsx
+++ components/evaluation.component.tsx
@@ -4,11 +4,11 @@ import { Card } from '@nextui-org/card';
 export const Evaluation = ({ evaluation }) => {
     return (
       <>
-        <Card className="grid grid-cols-3 w-full p-2">
-						<span className="text-sm text-center">강의명: { evaluation.lecture }</span>
-						<span className="text-sm text-center">교수: { evaluation.professor }</span>
-						<span className="text-sm text-center">총평: { evaluation.description }</span>
-						<span className="text-sm text-center">총점: { evaluation.totalRate }점</span>
+        <Card className="grid grid-cols-3 w-full mt-2 p-2">
+						<span className="text-sm text-center">강의명: { evaluation.nameOfLecture }</span>
+						<span className="text-sm text-center">교수: { evaluation.nameOfProfessor }</span>
+						<span className="text-sm text-center">{ evaluation.totalRate }점</span>
+            <p>총평: { evaluation.description }</p>
         </Card>
       </>
     );
