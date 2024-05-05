bf7b008b67a2a668a9c9e0bb3c80005cecdb0a58
JinhyeokBang <jinhyeokfang@gmail.com>
2024-05-05 11:36:11 +0900

    feat: 순서 변경

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
 		<>
 		<h1 className="text-2xl mb-2 font-bold text-center w-full">앱 메뉴</h1>
 		<div className="flex flex-wrap gap-4 justify-center">
+			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/evaluation')}>
+				<h1 className="text-2xl">강의평가</h1>
+			</Card>
 			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/timetable')}>
 				<h1 className="text-2xl">시간표</h1>
 			</Card>
@@ -20,9 +23,6 @@ export default function Home() {
 			</Card>
 			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/calculator')}>
 				<h1 className="text-2xl">학점계산기</h1>
-			</Card>
-			<Card className="flex-col items-center justify-center w-[45%] p-4 cursor-pointer bg-primary-400 text-white shadow-2xl" isPressable onClick={() => push('/evaluation')}>
-				<h1 className="text-2xl">강의평가</h1>
 			</Card>
 		</div>
 		<Card className="flex-col items-center justify-center my-6 w-full">
